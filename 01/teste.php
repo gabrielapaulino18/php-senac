877f42abe7445720d6dc3bc3ccdf2a32d70937fc
Gabriela Paulino <gabrielappsilva18@gmail.com>
2024-03-20 22:06:47 -0300

    01 - Configuração de ambiente

---
+++ 01/teste.php
@@ -0,0 +1,29 @@
+<?php
+
+echo "Olá Mundo!";
+
+// Isso é um comentário
+
+# Isso também é um comentário
+
+/* Isso é um comentário de múltiplas linhas
+eu disse, múltiplas linhas
+*/
+
+// Declaração de variáveis
+$nome = ' João';
+$sobrenome = 'Silva';
+$nome_completo;
+
+// echo $nome;
+// echo $sobremone;
+
+echo $nome. " " . $sobrenome . "<br>";
+echo $nome_completo . "<br>";
+
+// Variáveis numéricas
+$idade = 25 . "<br>";
+$ano = 2024;
+
+echo $idade;
+?>
