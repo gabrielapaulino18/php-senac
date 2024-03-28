fad5b20a6002801b4fca89d5f4b1afa9486c2354
Gabriela Paulinol <gabrielappsilva18@gmail.com>
2024-03-27 22:13:21 -0300

    07 - Condicionais concluídas

---
+++ 08/index.php
@@ -0,0 +1,19 @@
+<?php
+
+    //// ESTRUTURAS DE REPETIÇÃO EM PHP
+
+    //Exemplo 1: FOR
+    echo "Exemplo 1: FOR <br>";
+    for ($i = 1; $i <= 5 ; $i++) {
+        echo "Numero $i <br>";
+    }
+    echo "<br>";
+
+    // Exemplo 2: While
+    echo "Exemplo 2: While <br>";
+
+    $contador = 1;
+    
+
+
+?>
