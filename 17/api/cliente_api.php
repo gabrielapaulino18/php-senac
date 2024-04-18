444cf0983e57fd06a79963f3a728445b83b2c51b
Gabriela Paulino <gabrielappsilva18@gmail.com>
2024-04-18 20:58:58 -0300

    17 - Conta listar todas as APIs

--- 17/api/cliente_api.php
+++ 17/api/cliente_api.php
@@ -8,6 +8,15 @@ switch($action) {
     case 'listar':
         listarClientes();
         break;
+    case 'buscar':
+        buscarClientePorId();
+        break;
+    case 'cadastrar':
+        cadastrarCliente();
+        break;
+    case 'excluir':
+        excluirCliente();
+        break;
     default:
         http_response_code(400); // Requisição inválida
         echo json_encode(['error' => 'Ação inválida']);
@@ -31,5 +40,5 @@ function atualizarCliente() {
 }
 
 function excluirCliente() {
-    
+
 }
