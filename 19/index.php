281539022f6c5fb5c306daab21c85126ba42087c
Gabriela Paulino <gabrielappsilva18@gmail.com>
2024-04-25 20:38:53 -0300

    19 - Repositório de Produtos

--- 19/index.php
+++ 19/index.php
@@ -1,11 +1,15 @@
 <?php
 require_once 'database/PedidoRepository.php';
+require_once 'database/ProdutoRepository.php';
 
 $action = $_GET['action'];
 
 switch($action) {
     case 'listar':
         echo json_encode(PedidoRepository::getAllPedidos());
+        break;
+    case 'listarProdutos':
+        echo json_encode(ProdutoRepository::getAllProdutos());
         break;
     default:
         break;
